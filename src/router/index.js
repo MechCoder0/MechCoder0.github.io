ad825b04abbf793cd76cd45bc675918e17ca8f05
Isaac Meck <icedragonzclawz@gmail.com>
2022-01-29 14:35:11 -0500

    refactored the router

--- src/router/index.js
+++ src/router/index.js
@@ -1,50 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Computers from '../views/Computers.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/computers',
-    name: 'computers',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: Computers
-  },
-  {
-    path: '/cellphones',
-    name: 'cellphones',
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/Cellphones.vue')
-    }
-  },
-  {
-    path: '/breakout',
-    name: 'breakout',
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/BreakoutView.vue')
-    }
-  },
-  {
-    path: '/resources',
-    name: 'resources',
-    component: function(){
-      return import('../views/Resources.vue')
-    }
-  }
-
+  getRoute('', import('../views/Home.vue')),
+  getRoute('home', import('../views/Home.vue')),
+  getRoute('computers', import('../views/Computers.vue')),
+  getRoute('cellphones', import('../views/Cellphones.vue')),
+  getRoute('breakout', import('../views/BreakoutView.vue')),
+  getRoute('resources', import('../views/Resources.vue'))
 ]
+function getRoute(name, c){
+  return {
+    path: "/" + name,
+    name: name,
+    component: function(){return c}
+  }
+}
 
-const router = new VueRouter({
-  routes
-})
+const router = new VueRouter({routes})
 
 export default router
