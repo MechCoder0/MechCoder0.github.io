d5cb156ce2d3c1561b4dcf8d31eefd993cdb1542
Isaac Meck <icedragonzclawz@gmail.com>
2022-06-13 22:18:29 -0400

    added coffee!

--- src/views/Cellphones.vue
+++ src/views/Cellphones.vue
@@ -2,49 +2,70 @@
 	<div>
 		<BODY>
 			<div class="article">
-				<h1> Cell Phones!</h1>
+				<h1> Coffee Time!</h1>
 				<div>
 					<p>
-						If you are looking to buy a phone, one of the first things 
-						you need to know about cellphones is that there are two
-						different cellular networks:
-					</p>
-					<h2>CDMA</h2>
-					<p class="indent">
-						The CDMA network is used by Sprint, Verizon, and U.S. Cellular.
-					</p>
-					<h2>GSM</h2>
-					<p class="indent">
-						The GSM network is used by AT&T, T-Mobile, and pretty much everyone else. As a note, 
-						since Cricket Wireless is owned by AT&T it uses GSM.
-					</p>
-					<h2>Unlocked Phones</h2>
-					<p class="indent">
-						The second thing to know when purchasing a cellphone is what an 
-						unlocked phone is.
+						It’s no secret I love coffee. I drink roughly my weight in caffeine every morning. 
+						This leads to no problems and is great for my health. 
+						Every morning is pretty much the same: I wake up, regret existence, eventually get up, 
+						brush my teeth, use the toilet, eat an apple, and make mugs of coffee. 
+						This routine I have has been pretty much the same for some time. When I got married, 
+						the only thing that changed was the amount of coffee I had to share.  
 					</p>
 					<p class="indent">
-						Basically an unlocked phone is a phone that can be used for any
-						carrier on one of the networks. A phone is usually only unlocked for
-						GSM <b>OR</b> CDMA. Make sure to check which network it is unlocked for
-						before purchasing a phone and finding out that your GSM unlocked phone 
-						doesn't work with your current Sprint plan.
+						Being married, one cannot drink the highest tier of coffee every day, 
+						because that causes poordom. I do not wish to be poor, so I devised a tier system for our coffee. 
+						We drink mostly tier 2-3 coffee, and have tier 1 coffee on hand. 
+						We usually end up drinking the tier 1 coffee because it's 
+						getting too old though. We don't mind. 
+						Here is a rundown of our tier lists of coffee. 
 					</p>
-					<h2>Operating Systems</h2>
+					<h2>Tier 1</h2>
 					<p class="indent">
-						Another thing to know about cellphones is what Operating System (OS) 
-						you would like to use. The OS is how you interact with the phone. The
-						OS will, to some extent, determain what apps you can download.
-						There are three major operating systems for cell phones.
+						The God level. Coffee so good, you would not mind serving it to Jesus Himself, should he decide to visit some morning. 
+						Here is where the whole bean, single origin, blessing from heaven exists. 
+						This coffee usually runs about $15-20 /12 oz. That's right, it is so good you ca not even buy it 
+						by the pound. That's how you know.
 					</p>
-					<div class="indent">
-						<ul>
-							<li>OSX (iPhones): Generally the most expensive. Very simple layout</li>
-							<li>Android: Very modifiable, used on a lot of different cellphones</li>
-							<li>Windows Phone: Only used on windows phones. Windows 8 was based off
-								this OS</li>
-						</ul>
-					</div>
+					<h2>Tier 2</h2>
+					<p class="indent">
+						The middle-class level. Decent coffee.  
+						Nothing to write home about. For example, I would not write, 
+						"Dear mom, today I had a middle-class cup of coffee! It was completely adequate and exactly what I expected. 
+						I brewed this mediocrity with my mediocre drip machine. Please send money."
+						But that is not to say this coffee is bad! It just is decent. 
+					</p>
+					<h2>Tier 3</h2>
+					<p class="indent">
+						The “it's not bad, I enjoy it because it cost me less than dirt” level. 
+						This level of coffee is usually bought by the case. 
+						For roughly $1. But it is still serviceable! 
+						This coffee should not be drunken with the expectancy of life change, 
+						but it should still get the job done when you unexpectantly have your entire 20-person family visiting and they all want the Joe. 
+					</p>
+					<h2>Tier 4</h2>
+					<p class="indent">
+						The “I do not drink this unless I must" level. 
+						This coffee is from church foyers and wedding receptions. 
+						Notable mentions here are Folgers and Maxwell house. 
+						Something that should be known, is these gallons of dirt 
+						are not even 100% coffee! They only have to be roughly 60% coffee or some such legally. 
+						Think twice before drinking. You have been warned.
+					</p>
+					<h2>Tier 5</h2>
+					<p class="indent">
+						The “Please don't make me” level. 
+						This coffee is mostly reserved for instant coffee. 
+						I believe this coffee was not ever meant to be consumed but 
+						instead was a joke a sales team got a hold of. 
+						This is the coffee the devil himself orders when he goes out. 
+						Best not to be seen with it.
+					</p>
+					<p class="indent">
+						These are our coffee levels! 
+						I hope you have gained insight into the wonderful leveling of the dark brew. 
+						May your day be blessed from the reading.
+					</p>
 				</div>
 			</div>
 		</BODY>
