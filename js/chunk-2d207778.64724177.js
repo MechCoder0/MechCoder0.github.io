84f4ad048da1a90fe1bec038b00373f7f82718ea
Isaac Meck <icedragonzclawz@gmail.com>
2022-07-24 17:49:15 -0400

    updated article to be a component. Also removed unnecessary files

---
+++ js/chunk-2d207778.64724177.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d207778"],{a13e:function(e,n,t){"use strict";t.r(n);var l=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},c=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("article",[t("h1",[e._v("hello")])])}],r=t("2877"),u={},a=Object(r["a"])(u,l,c,!1,null,null,null);n["default"]=a.exports}}]);
+//# sourceMappingURL=chunk-2d207778.64724177.js.map
